dc1f677e5dba2536976b0c7ac9f1ab7111148564
Martain24 <vilaboamartin@gmail.com>
2025-02-18 13:09:09 +0100

    Artifact assigment done! Close #3

--- src/test/java/com/mvilaboa/hogwarts_artifacts_online/artifactv2/ArtifactServiceTest.java
+++ src/test/java/com/mvilaboa/hogwarts_artifacts_online/artifactv2/ArtifactServiceTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 
 import com.mvilaboa.hogwarts_artifacts_online.artifact.Artifact;
